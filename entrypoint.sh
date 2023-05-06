f3a5d2d9e3e4d0944e6f841ccf18177686d6dd37
iengs <124105988+iengs@users.noreply.github.com>
2023-05-06 03:54:32 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+UUID=${UUID:-'8846d132-2b68-4167-adc2-a691e1a55e17'}
 
 # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
 NEZHA_SERVER= 
@@ -10,8 +10,8 @@ NEZHA_PORT=
 NEZHA_KEY=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"e1fe518fa36645c5cea82602681f902c","TunnelSecret":"6Vgd0gAlPonEJS+nFHEBHJQnS2q835t4XR7oai9bVCI=","TunnelID":"e08b915f-53b7-4923-b2a1-d2011d70827a"}'
+ARGO_DOMAIN=bwide4.ndykywcjhrw.cf
 
 generate_config() {
   cat > config.json << EOF
